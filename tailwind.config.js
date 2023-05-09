6100e86085cad563586daa34d3e8101ea2d0d59e
Pierre Magnien <magniendcpierre@gmail.com>
2023-05-09 16:42:36 +0200

    tailwind confi + fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,51 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily:{
+      'main': ['Noto', 'sans-serif'],
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors:{
+        blue:{
+          extralight:'#F3F2F8',
+          lighter:'#CACBDF',
+          light:'#71739F',
+          base :'#374077',
+          dark:'#161C3D'
+        },
+        grey:{
+          lighter:'#DDDDDD',
+          base :'#C1C1C1',
+          dark:'#AAAAAA'
+        },
+        red: {
+          lighter:'#E2A4A4',
+          light:'#CA6A69',
+          base :'#C62120',
+          dark:'#9D1414'
+        },
+        black:"#121525",
+        white:"#ffffff",
+      },
+      spacing:{
+        1:'3px',
+        2:'6px',
+        3:'9px',
+        4:'12px',
+        5:'15px',
+        6:'18px',
+        7:'21px',
+        lg:'45px',
+        xl:'87px',
+      },
+      fontSize:{
+        sm:'16px',
+        base: '17px',
+        lg:'22px',
+        xl:'26px',
+        xxl:'28px',
+        subheading:'40px',
+        heading:'56px'
       },
     },
   },
