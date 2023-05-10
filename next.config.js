cceccd36e152c5f63b008c9903a8ec342d5117b9
Pierre Magnien <magniendcpierre@gmail.com>
2023-05-10 09:56:06 +0200

    tailwind layers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    eslint: {
+        dirs: ['src'],
+    },
+
+    reactStrictMode: true,
+    swcMinify: true,
+}
 
 module.exports = nextConfig
