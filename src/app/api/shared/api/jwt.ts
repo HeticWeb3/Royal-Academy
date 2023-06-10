f8650e96b20b8c2fcfda08307b07060022e4de72
Jérémy <jeremyschneider21@gmail.com>
2023-06-11 01:46:03 +0200

    verify logged user with the middleware

--- src/app/api/shared/api/jwt.ts
+++
@@ -1,7 +0,0 @@
-import jwt from "jsonwebtoken"
-
-export const generateTokens = (userID: number, expires: {'expireAccess': string, 'expireRefresh': string}) => {
-    const accessToken: string =  jwt.sign({id: userID}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: expires.expireAccess})
-    const refreshToken: string =  jwt.sign({id: userID}, process.env.REFRESH_TOKEN_SECRET, {expiresIn: expires.expireAccess})
-    return [accessToken, refreshToken]
-}
