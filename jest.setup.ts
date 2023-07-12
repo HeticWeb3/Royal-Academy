02bc3b213729017620f5e743a4a888fe75943340
Jérémy <jeremyschneider21@gmail.com>
2023-07-12 09:09:08 +0200

    fix afterAll

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,4 +1,10 @@
 const util = require('util');
 const exec =  util.promisify(require('child_process').exec);
 
-afterAll(() => exec('npx prisma migrate reset --force --skip-generate'));
+afterAll(async () => {
+    try {
+        await exec('npx prisma migrate reset --force --skip-generate')
+    } catch(e: any) {
+        return
+    }
+});
