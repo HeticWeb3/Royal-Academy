eccb66ea7d470a4a2e73a6560728e0b2b30bbb34
Pierre MDC <magniendcpierre@gmail.com>
2023-09-16 13:59:21 +0200

    add grid layout to desktop

--- src/app/Components/Organisms/Course/Course.tsx
+++ src/app/Components/Organisms/Course/Course.tsx
@@ -13,7 +13,7 @@ const Course = (data: { data: coursesDataProps }) => {
         <div className={' flex flex-col gap-7 lg:gap-[6] col-span-full'}>
             <div
                 style={{backgroundImage: `url(${backgroundImageUrl})`}}
-                className={ `bg-cover bg-center aspect-video flex flex-col justify-center items-centers -mx-[15px] -mx-[77px]`}>
+                className={ `bg-cover bg-center aspect-video flex flex-col justify-center items-centers -mx-[15px] lg:-mx-[77px]`}>
                 <h1 className={'text-lg uppercase lg:text-heading lg:mb-lg text-center'}>{singleCourses.name}</h1>
             </div>
 
@@ -32,25 +32,27 @@ const Course = (data: { data: coursesDataProps }) => {
                 }
 
             </div>
-            <div className={'lg:mb-[120px]'}>
-                <p className={'text-center lg:w-2/3 mx-auto'}>{singleCourses.description}</p>
-            </div>
+            <div className={'lg:grid lg:grid-cols-12'}>
+                <div className={'lg:mb-[120px] col-span-8'}>
+                    <p className={'text-center lg:w-2/3 mx-auto'}>{singleCourses.description}</p>
+                </div>
 
-            <div className={'mb-7'}>
-                <h2 className={'text-lg mb-7'}>ALL LESSONS</h2>
-                <div className={'flex flex-row flex-nowrap gap-7 overflow-x-scroll no-scrollbar mx-[-15px] px-[15px]'}>
-                    {lessons?.map((lesson: Record<string, any>) => {
-                        return (
-                            <Link href={`/lesson/${lesson.id}`}
-                                key={'lesson-'+lesson.id}
-                                className={'max-w-[120px]'} >
-                                <div
-                                    style={{backgroundImage: `url(${lesson.imageUrl})`}}
-                                    className={'w-[120px] h-[70px] bg-red-light rounded-sm bg-center bg-cover'}></div>
-                                <h3 className={'truncate text-center'}>{lesson.nom}</h3>
-                            </Link>
-                        )
-                    })}
+                <div className={'mt-7 lg:mt-0 mb-7 col-span-4'}>
+                    <h2 className={'text-lg mb-7'}>ALL LESSONS</h2>
+                    <div className={'flex flex-row flex-nowrap lg:flex-wrap gap-7 overflow-x-scroll no-scrollbar mx-[-15px] px-[15px]'}>
+                        {lessons?.map((lesson: Record<string, any>) => {
+                            return (
+                                <Link href={`/lesson/${lesson.id}`}
+                                    key={'lesson-'+lesson.id}
+                                    className={'max-w-[120px]'} >
+                                    <div
+                                        style={{backgroundImage: `url(${lesson.imageUrl})`}}
+                                        className={'w-[120px] h-[70px] bg-red-light rounded-sm bg-center bg-cover'}></div>
+                                    <h3 className={'truncate text-center'}>{lesson.nom}</h3>
+                                </Link>
+                            )
+                        })}
+                    </div>
                 </div>
             </div>
         </div>
