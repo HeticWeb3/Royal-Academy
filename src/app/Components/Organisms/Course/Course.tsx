e32042a7c4ea76bcc1efcae670636384c20fc977
Pierre MDC <magniendcpierre@gmail.com>
2023-09-16 17:48:40 +0200

    add level & instrumlent

--- src/app/Components/Organisms/Course/Course.tsx
+++ src/app/Components/Organisms/Course/Course.tsx
@@ -8,6 +8,8 @@ const Course = (data: { data: coursesDataProps }) => {
     const [professors,setProfessors] = useState<any>(null);
     const backgroundImageUrl = singleCourses.imageUrl
 
+    console.log(singleCourses)
+
     return (
 
         <div className={' flex flex-col gap-7 lg:gap-[6] col-span-full'}>
@@ -16,12 +18,11 @@ const Course = (data: { data: coursesDataProps }) => {
                 className={ `bg-cover bg-center aspect-video flex flex-col justify-center items-centers -mx-[15px] lg:-mx-[77px]`}>
                 <h1 className={'text-lg uppercase lg:text-heading lg:mb-lg text-center'}>{singleCourses.name}</h1>
             </div>
-
-            <Link href={`/lesson/${lessons[0].id}`} className={'flex justify-center my-lg'}>
-                <button className={'button bg-white text-blue-light w-full lg:w-1/4 hover:bg-blue-light hover:text-white transition-colors'}>
-                    Start the lessons
-                </button>
-            </Link>
+            <div className={'flex justify-center my-lg'}>
+                <Link href={`/lesson/${lessons[0].id}`} className={' button text-center bg-white text-blue-light w-full lg:w-1/4 hover:bg-blue-light hover:text-white transition-colors'}>
+                Start the lessons
+                </Link>
+            </div>
 
 
             <div className={'text'}>
@@ -32,12 +33,16 @@ const Course = (data: { data: coursesDataProps }) => {
                 }
 
             </div>
-            <div className={'lg:grid lg:grid-cols-12'}>
-                <div className={'lg:mb-[120px] col-span-8'}>
-                    <p className={'text-center lg:w-2/3 mx-auto'}>{singleCourses.description}</p>
+            <div className={'lg:grid lg:gap-7 lg:grid-cols-12'}>
+                <div className={'lg:mb-[120px] col-span-8 flex flex-col gap-lg'}>
+                    <p className={'text-center'}>{singleCourses.level.nom} - {singleCourses.instrument.name}</p>
+                    <div>
+                        <h2 className={'lg:text-lg lg:mb-lg  text-lg'}>DESCRIPTION</h2>
+                        <p className={'text-center lg:text-left'}>{singleCourses.description}</p>
+                    </div>
                 </div>
 
-                <div className={'mt-7 lg:mt-0 mb-7 col-span-4'}>
+                <div className={'mt-7 lg:mt-0 mb-7 lg:col-start-10 lg:col-end-13'}>
                     <h2 className={'text-lg mb-7'}>ALL LESSONS</h2>
                     <div className={'flex flex-row flex-nowrap lg:flex-wrap gap-7 overflow-x-scroll no-scrollbar mx-[-15px] px-[15px]'}>
                         {lessons?.map((lesson: Record<string, any>) => {
